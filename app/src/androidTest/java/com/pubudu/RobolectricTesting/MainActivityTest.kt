9df5bc33ab72a8b7d599ce3d1e8540179c470a90
PubuduWickramathunge <pubuduwickramathunge99@gmail.com>
2022-09-06 16:03:09 +0530

    Robolectric Testing

---
+++ app/src/androidTest/java/com/pubudu/RobolectricTesting/MainActivityTest.kt
@@ -0,0 +1,27 @@
+package com.pubudu.RobolectricTesting
+
+import android.R
+import android.widget.TextView
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+
+@RunWith(RobolectricTestRunner::class)
+class MainActivityTest {
+    val textShouldPresent = "Hello World"
+    private lateinit var activity: MainActivity
+
+    @Before
+    fun setup() {
+        activity = Robolectric.setupActivity(MainActivity::class.java)
+    }
+    @Test
+    fun is_hello_world_text_present() {
+
+        val textView = activity.findViewById(R.id.textview_first) as TextView
+        assertTrue("MyRestaurants" == appNameTextView.text.toString())
+    }
+}
